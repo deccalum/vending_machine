503f5cf091fd20838799ed2fa02062f763526795
Dennis Calmius <dcalmius@gmail.com>
2025-12-29 12:52:25 +0100

    tests

--- src/test/java/lexicon/se/VendingMachineImplTest.java
+++ src/test/java/lexicon/se/VendingMachineImplTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class VendingMachineImplTest {
+    VendingMachineImpl vendingMachine = new VendingMachineImpl();
+    
 
     @Test
     void insertCoin() {
+        vendingMachine.insertCoin(10);
+        vendingMachine.insertCoin(30);
+        vendingMachine.insertCoin(10);
+        vendingMachine.insertCoin(50);
     }
 
     @Test
@@ -16,6 +22,20 @@ class VendingMachineImplTest {
 
     @Test
     void purchaseProduct() {
+
+        // The assertEquals commands verify that actual values match expected values. If
+        // they don't match, the test fails.
+
+
+        assertEquals(0, vendingMachine.getBalance());
+
+        vendingMachine.insertCoin(50);
+        Product product = vendingMachine.purchaseProduct(2);
+
+        assertNotNull(product);
+        assertEquals(0, vendingMachine.getBalance());
+        assertEquals(2, product.getQuantity());
+
     }
 
     @Test
