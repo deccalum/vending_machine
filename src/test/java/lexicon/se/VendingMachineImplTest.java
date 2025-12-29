0399935998863f1ba8d3bc366e05060af66630ef
Dennis Calmius <dcalmius@gmail.com>
2025-12-29 12:20:53 +0100

    plain test

---
+++ src/test/java/lexicon/se/VendingMachineImplTest.java
@@ -0,0 +1,28 @@
+package lexicon.se;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VendingMachineImplTest {
+
+    @Test
+    void insertCoin() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void purchaseProduct() {
+    }
+
+    @Test
+    void returnChange() {
+    }
+
+    @Test
+    void getProducts() {
+    }
+}
